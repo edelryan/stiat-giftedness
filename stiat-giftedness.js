3015074965c08d94c1de210149ce9ccae2ce51a7
edelryan <edelniriain@gmail.com>
2025-07-20 20:52:28 +0100

    Update stiat-giftedness.js

--- stiat-giftedness.js
+++ stiat-giftedness.js
@@ -1,67 +1,63 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
-	
-	var API = new APIConstructor();
-		  return stiatExtension({
-		  category : { 
-		    name : 'Black people', //Will appear in the data.
-		    title : {
-		      media : {word : 'Black people'}, //Name of the category presented in the task.
-		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-		      height : 7 //Used to position the "Or" in the combined block.
-		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#31b404','font-size':'3em'}
-		  },	
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.3/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
+    var API = new APIConstructor();
 
-  		attribute1 : 
-			{
-			name : 'Unpleasant', //Attribute label
-			title : {
-				media : {word : 'Negative'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Bomb'},
-				{word: 'Abuse'},
-				{word: 'Sadness'},
-				{word: 'Pain'},
-				{word: 'Poison'},
-				{word: 'Grief'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
-		attribute2 : 
-			{
-			name : 'Pleasant', //Attribute label
-			title : {
-				media : {word : 'Positive'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Paradise'},
-				{word: 'Pleasure'},
-				{word: 'Cheer'},
-				{word: 'Wonderful'},
-				{word: 'Splendid'},
-				{word: 'Love'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
+    return stiatExtension({
+        category: {
+            name: 'Gifted Students',
+            title: {
+                media: { word: 'Gifted' },
+                css: { color: '#31b404', 'font-size': '2em' },
+                height: 7
+            },
+            media: [
+                { word: 'Bright' },
+                { word: 'Talented' },
+                { word: 'Exceptional' },
+                { word: 'Gifted' },
+                { word: 'Advanced' },
+                { word: 'High-achieving' }
+            ],
+            css: { color: '#31b404', 'font-size': '3em' }
+        },
 
-  base_url : {//Where are your images at?
-    image : 'https://baranan.github.io/minno-tasks/images/'
-  }}
-  );
-  });
+        attribute1: {
+            name: 'Unpleasant',
+            title: {
+                media: { word: 'Negative' },
+                css: { color: '#31b404', 'font-size': '2em' },
+                height: 7
+            },
+            media: [
+                { word: 'Difficult' },
+                { word: 'Problem' },
+                { word: 'Burden' },
+                { word: 'Frustrating' },
+                { word: 'Demanding' },
+                { word: 'Disruptive' }
+            ],
+            css: { color: '#31b404', 'font-size': '3em' }
+        },
+
+        attribute2: {
+            name: 'Pleasant',
+            title: {
+                media: { word: 'Positive' },
+                css: { color: '#31b404', 'font-size': '2em' },
+                height: 7
+            },
+            media: [
+                { word: 'Helpful' },
+                { word: 'Motivating' },
+                { word: 'Inspiring' },
+                { word: 'Rewarding' },
+                { word: 'Exciting' },
+                { word: 'Encouraging' }
+            ],
+            css: { color: '#31b404', 'font-size': '3em' }
+        },
+
+        base_url: {
+            image: '' // not needed unless you're using images
+        }
+    });
+});
