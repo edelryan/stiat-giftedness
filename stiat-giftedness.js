22b3542b71a2ff7e8c68d6cac2db8609d537c43c
edelryan <edelniriain@gmail.com>
2025-07-20 21:58:58 +0100

    Update stiat-giftedness.js

--- stiat-giftedness.js
+++ stiat-giftedness.js
@@ -1,67 +1,65 @@
 define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/stiat/qualtrics/qstiat6.js'], function(APIConstructor, stiatExtension){
 	
 	var API = new APIConstructor();
-		  return stiatExtension({
-		  category : { 
-		    name : 'Black people', //Will appear in the data.
-		    title : {
-		      media : {word : 'Black people'}, //Name of the category presented in the task.
-		      css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-		      height : 7 //Used to position the "Or" in the combined block.
-		    }, 
-		    media : [ //Stimuli content as PIP's media objects
-    		    	{image : 'black1.jpg'}, 
-    			{image : 'black2.jpg'}, 
-    			{image : 'black3.jpg'}, 
-    			{image : 'black4.jpg'}, 
-    			{image : 'black5.jpg'}, 
-    			{image : 'black6.jpg'}
-		    ], 
-		    //Stimulus css (style)
-		    css : {color:'#31b404','font-size':'3em'}
-		  },	
 
-  		attribute1 : 
-			{
-			name : 'Unpleasant', //Attribute label
-			title : {
-				media : {word : 'Negative'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Bomb'},
-				{word: 'Abuse'},
-				{word: 'Sadness'},
-				{word: 'Pain'},
-				{word: 'Poison'},
-				{word: 'Grief'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
-		attribute2 : 
-			{
-			name : 'Pleasant', //Attribute label
-			title : {
-				media : {word : 'Positive'}, //Name of the category presented in the task.
-				css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
-				height : 7 //Used to position the "Or" in the combined block.
-			}, 
-			media : [ //Stimuli
-				{word: 'Paradise'},
-				{word: 'Pleasure'},
-				{word: 'Cheer'},
-				{word: 'Wonderful'},
-				{word: 'Splendid'},
-				{word: 'Love'}
-			], 
-			//Can change color and size of the targets here.
-			css : {color:'#31b404','font-size':'3em'}
-			},
+	return stiatExtension({
 
-  base_url : {//Where are your images at?
-    image : 'https://baranan.github.io/minno-tasks/images/'
-  }}
-  );
-  });
+		category: {
+			name: 'Gifted Students',
+			title: {
+				media: {word: 'Gifted Students'},
+				css: {color:'#31b404','font-size':'2em'},
+				height: 7
+			},
+			media: [
+				{word: 'Gifted'},
+				{word: 'Bright'},
+				{word: 'Talented'},
+				{word: 'Intelligent'},
+				{word: 'Advanced'},
+				{word: 'Exceptional'}
+			],
+			css: {color:'#31b404','font-size':'3em'}
+		},
+
+		attribute1: {
+			name: 'Negative',
+			title: {
+				media: {word: 'Negative'},
+				css: {color:'#31b404','font-size':'2em'},
+				height: 7
+			},
+			media: [
+				{word: 'Arrogant'},
+				{word: 'Isolated'},
+				{word: 'Awkward'},
+				{word: 'Weird'},
+				{word: 'Entitled'},
+				{word: 'Selfish'}
+			],
+			css: {color:'#31b404','font-size':'3em'}
+		},
+
+		attribute2: {
+			name: 'Positive',
+			title: {
+				media: {word: 'Positive'},
+				css: {color:'#31b404','font-size':'2em'},
+				height: 7
+			},
+			media: [
+				{word: 'Motivated'},
+				{word: 'Curious'},
+				{word: 'Creative'},
+				{word: 'Helpful'},
+				{word: 'Hardworking'},
+				{word: 'Friendly'}
+			],
+			css: {color:'#31b404','font-size':'3em'}
+		},
+
+		base_url: {
+			image: 'https://baranan.github.io/minno-tasks/images/' // Not used here, but required field
+		}
+	});
+});
